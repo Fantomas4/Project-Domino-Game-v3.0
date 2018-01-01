632aef8c3a83771e5a47062df93cb41d355f7e16
Fantomas4 <stefanokf@csd.auth.gr>
2018-01-01 06:52:26 +0200

    Continued work on the Swing GUI for the game. Finishing up work on the Solo1 GUI, Hungarian GUI and AllSeven GUI. Started work on Localization/Internationalization. Started work on Javadoc documentation

--- src/dominogamev3/Human.java
+++ src/dominogamev3/Human.java
@@ -10,16 +10,25 @@ package dominogamev3;
  * @author Sierra Kilo
  */
 public class Human extends Player {
-    
+
+    /**
+     * This class contains the necessary logic code and methods that are used to
+     * create and manage the Human Player object representing the human player
+     * participating in the game session.
+     *
+     * @param name the name of the human player
+     * @param tilesAmount the amount of tiles to be given to the human player at the start of the game.
+     * @param heap the object of the heap the game uses.
+     */
     public Human(String name, int tilesAmount, Heap heap) {
-        
+
         super();
 
         this.name = name;
         this.heap = heap;
 
         givePlayerTiles(tilesAmount); //gives player the needed amount of tiles
-                                      //for the game (tilesAmount).
+        //for the game (tilesAmount).
     }
-    
+
 }
